1f30060c1fd5ba60a717fe8a57061409d96c0aa4
yuuki-fuji <yuukikun666@gmail.com>
2023-05-17 19:02:23 +0900

    first commit

---
+++ site.config.js
@@ -0,0 +1,71 @@
+const CONFIG = {
+  // profile setting (required)
+  profile: {
+    name: "yu-ki",
+    image: "/artist.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    role: "エンジニア",
+    bio: "宜しくお願い致します。",
+    email: "",
+    linkedin: "",
+    github: "yuuki-fuji",
+    instagram: "",
+  },
+  projects: [
+    {
+      name: `準備中`,
+      href: "",
+    },
+  ],
+  // blog setting (required)
+  blog: {
+    title: "Notion-Blog",
+    description: "welcome!",
+    theme: "auto", // ['light', 'dark', 'auto']
+  },
+
+  // CONFIG configration (required)
+  link: "",
+  since: 2023, // If leave this empty, current year will be used.
+  lang: "ja-JP", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
+  seo: {
+    keywords: ["Blog", "Website", "Notion"],
+  },
+
+  // notion configuration (required)
+  notionConfig: {
+    pageId: process.env.NOTION_PAGE_ID,
+  },
+
+  // plugin configuration (optional)
+  googleAnalytics: {
+    enable: false,
+    config: {
+      measurementId: process.env.NEXT_PUBLIC_GOOGLE_MEASUREMENT_ID || "",
+    },
+  },
+  googleSearchConsole: {
+    enable: false,
+    config: {
+      siteVerification: process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION || "",
+    },
+  },
+  utterances: {
+    enable: false,
+    config: {
+      repo: "morethanmin/morethan-log",
+      "issue-term": "og:title",
+      label: "💬 Utterances",
+    },
+  },
+  cusdis: {
+    enable: false,
+    config: {
+      host: "https://cusdis.com",
+      appid: "", // Embed Code -> data-app-id value
+    },
+  },
+  isProd: process.env.VERCEL_ENV === "production", // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
+}
+
+module.exports = { CONFIG }
