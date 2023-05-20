7994dd62f3d40d334ba64fc109e332f4f9958ab6
fujimoto <yuukikun666@gmail.com>
2023-05-20 21:14:27 +0900

    Googleサーチコンソールの有効化

--- site.config.js
+++ site.config.js
@@ -47,7 +47,7 @@ const CONFIG = {
     },
   },
   googleSearchConsole: {
-    enable: false,
+    enable: true,
     config: {
       siteVerification: process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION || "",
     },
